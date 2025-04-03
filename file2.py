7fb93419115a041044cb0fb9036351f11ada6dea
aha3608 <50081696+aha3608@users.noreply.github.com>
2025-04-03 10:13:44 -0400

    Update file2.py

--- file2.py
+++ file2.py
@@ -1,1 +1,11 @@
+import csv
+
+# Open the CSV file
+with open('example.csv', mode='r') as file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(file)
+    
+    # Loop through each row in the CSV file and print it
+    for row in csv_reader:
+        print(row)
 
